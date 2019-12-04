a6c608b453b2b1afff57ee823f6ac98dbc0f8d97
Roberta Cucuzza <robertacucuzza@Robertas-MacBook-Air.local>
2019-12-04 12:20:34 +0000

    Add homework

---
+++ Payrate_test.py
@@ -0,0 +1,5 @@
+hours = input("How many hours have you worked this week? ")
+rate = input("What is your hourly rate? ")
+pay = float(hours)*float(rate)
+print("This week you will get paid:",pay)
+#just checking if the changes are reflected in GitHub
